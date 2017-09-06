f356f16c57f16aa79bd49fa2cf585049e606808a
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-09-06 20:39:02 +1000

    added bash code

--- bash_scripts/install_flask.sh
+++ bash_scripts/install_flask.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 # --------------------------------------------
-#
+# install required python modules
 # --------------------------------------------
 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y install python3-pip
+sudo -H pip3 install --upgrade pip
+
+sudo -H pip3 install gunicorn
+
+sudo -H pip3 install Flask
+sudo -H pip3 install Flask-Compress
+sudo -H pip3 install Flask-Cors
+
+sudo -H pip3 install psycopg2
