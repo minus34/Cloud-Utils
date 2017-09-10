e88c8626f01a3bda75f17a546e8c03ff44690abd
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-09-10 19:46:56 +1000

    added support for multiple schemas in pg_dump file

--- bash_scripts/postgres_restore.sh
+++ bash_scripts/postgres_restore.sh
@@ -4,9 +4,9 @@
 # Copies a pg_dump file from AWS S3, restores it to Postgres and grants read only access
 #
 # Input vars:
-#   0 = S3 bucket name
+#   0 = S3 bucket name (and folder name if not in the root folder)
 #   1 = pg_dump file name
-#   2 = target schema name
+#   2 = schema names (space delimited)
 #
 # ---------------------------------------------------------------------------------------
 
@@ -15,13 +15,16 @@ sudo mkdir ~/data
 sudo aws s3 cp s3://{0}/{1} ~/data/{1}
 sudo pg_restore -Fc -v -d postgres -p 5432 -U postgres -h localhost ~/data/{1}
 
-# grant read only user access to all tables & sequences
-sudo -u postgres psql -c "GRANT USAGE ON SCHEMA {2} TO rouser;" postgres
-sudo -u postgres psql -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA {2} TO rouser;" postgres
-sudo -u postgres psql -c "GRANT SELECT ON ALL TABLES IN SCHEMA {2} to rouser;" postgres
-sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA {2} GRANT SELECT ON SEQUENCES TO rouser;" postgres
-sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA {2} GRANT SELECT ON TABLES TO rouser;" postgres
-sudo -u postgres psql -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA {2} TO rouser;" postgres
+# grant read only user access to all tables & sequences for each schema in pg_dump file
+schemas=({2})
+
+for name in ${schemas[@]}; do
+    sudo -u postgres psql -c "GRANT USAGE ON SCHEMA ${name} TO rouser;" postgres
+    sudo -u postgres psql -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA ${name} TO rouser;" postgres
+    sudo -u postgres psql -c "GRANT SELECT ON ALL TABLES IN SCHEMA ${name} to rouser;" postgres
+    sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA ${name} GRANT SELECT ON SEQUENCES TO rouser;" postgres
+    sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA ${name} GRANT SELECT ON TABLES TO rouser;" postgres
+    sudo -u postgres psql -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${name} TO rouser;" postgres
 
 # delete dump files
 cd ~/data/
