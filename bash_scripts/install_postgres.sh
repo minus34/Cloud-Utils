7f89473bae4fddd15f9471c4e8b832cca03f9146
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-09-10 20:43:50 +1000

    added support for multiple IP ranges accessing server

--- bash_scripts/install_postgres.sh
+++ bash_scripts/install_postgres.sh
@@ -6,7 +6,7 @@
 # Input vars:
 #   0 = postgres user password
 #   1 = read-only user (rouser) password
-#   2 = CIDP IP range that can access the database server
+#   2 = CIDP IP range(s) that can access the database server - space delimited
 #
 # --------------------------------------------------------------------------------------
 
@@ -22,9 +22,10 @@ sudo DEBIAN_FRONTEND=noninteractive apt -q -y install postgis
 
 # alter postgres user and create database
 sudo -u postgres psql -c "ALTER USER postgres ENCRYPTED PASSWORD '{0}';"
-sudo -u postgres psql -c "CREATE EXTENSION adminpack;CREATE EXTENSION postgis;" postgres
+#sudo -u postgres psql -c "CREATE EXTENSION adminpack;CREATE EXTENSION postgis;" postgres
+sudo -u postgres psql -c "CREATE EXTENSION postgis;" postgres
 
-# create read only user and grant access to all tables & sequences
+# create read only user and grant access to all tables & sequences in public schema (to enable PostGIS use)
 sudo -u postgres psql -c "CREATE USER rouser WITH ENCRYPTED PASSWORD '{1}';" postgres
 sudo -u postgres psql -c "GRANT CONNECT ON DATABASE postgres TO rouser;" postgres
 sudo -u postgres psql -c "GRANT USAGE ON SCHEMA public TO rouser;" postgres
@@ -32,8 +33,17 @@ sudo -u postgres psql -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO rous
 sudo -u postgres psql -c "GRANT SELECT ON ALL TABLES IN SCHEMA public to rouser;" postgres
 sudo -u postgres psql -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO rouser;" postgres  # for PostGIS functions
 
-# alter whitelisted postgres clients (the VPC subnet and the test client IP address)
-sudo sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.6/main/postgresql.conf
-echo -e "host\t postgres\t rouser\t {2}\t md5" | sudo tee -a /etc/postgresql/9.6/main/pg_hba.conf
-#echo -e "host\t postgres\t rouser\t 101.164.227.2/32\t md5" | sudo tee -a /etc/postgresql/9.6/main/pg_hba.conf
+# whitelist postgres clients (if any)
+ip_ranges=({2})
+
+if [ ${#ip_ranges[@]} -gt 0 ]; then
+    # allow external access to postgres
+    sudo sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.6/main/postgresql.conf
+
+    # enable client IP range access for read-only user
+    for ip_range in ${ip_ranges[@]}; do
+        echo -e "host\t postgres\t rouser\t ${ip_range}\t md5" | sudo tee -a /etc/postgresql/9.6/main/pg_hba.conf
+    done
+fi
+
 sudo service postgresql restart
--- bash_scripts/postgres_restore.sh
+++ bash_scripts/postgres_restore.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# ---------------------------------------------------------------------------------------
-# Copies a pg_dump file from AWS S3, restores it to Postgres and grants read only access
+# --------------------------------------------------------------------------------------------------------
+# Copies a pg_dump file from AWS S3, restores it to Postgres and grants read only access to the schema(s)
 #
 # Input vars:
 #   0 = S3 bucket name (and folder name if not in the root folder)
 #   1 = pg_dump file name
-#   2 = schema names (space delimited)
+#   2 = schema name(s) - space delimited
 #
-# ---------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
 
-# restore data into database and assign privileges for read only user
+# restore schema(s)
 sudo mkdir ~/data
 sudo aws s3 cp s3://{0}/{1} ~/data/{1}
 sudo pg_restore -Fc -v -d postgres -p 5432 -U postgres -h localhost ~/data/{1}
 
-# grant read only user access to all tables & sequences for each schema in pg_dump file
+# grant read-only user access to all tables & sequences for each schema in the dump file
 schemas=({2})
-
 for name in ${schemas[@]}; do
     sudo -u postgres psql -c "GRANT USAGE ON SCHEMA ${name} TO rouser;" postgres
     sudo -u postgres psql -c "GRANT SELECT ON ALL SEQUENCES IN SCHEMA ${name} TO rouser;" postgres
@@ -25,6 +24,7 @@ for name in ${schemas[@]}; do
     sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA ${name} GRANT SELECT ON SEQUENCES TO rouser;" postgres
     sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA ${name} GRANT SELECT ON TABLES TO rouser;" postgres
     sudo -u postgres psql -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${name} TO rouser;" postgres
+done
 
 # delete dump files
 cd ~/data/
