df95b7ffb3139b308dee3ca1477f0c1ffba6c457
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-10-21 19:01:30 +1100

    nginx install testing

--- bash_scripts/install_nginx.sh
+++ bash_scripts/install_nginx.sh
@@ -6,11 +6,14 @@
 
 # add repo
 sudo add-apt-repository -y "deb http://nginx.org/packages/mainline/ubuntu/ xenial nginx"
-wget --quiet -O http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 
-## add repo to sources
-#sudo bash -c "echo 'deb http://nginx.org/packages/mainline/ubuntu/ xenial nginx' >> /etc/apt/sources.list"
-#sudo bash -c "echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ xenial nginx' >> /etc/apt/sources.list"
+# add repo to sources
+sudo bash -c "echo 'deb http://nginx.org/packages/mainline/ubuntu/ xenial nginx' >> /etc/apt/sources.list"
+sudo bash -c "echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ xenial nginx' >> /etc/apt/sources.list"
+
+# get GPG key
+sudo wget --quiet https://nginx.org/keys/nginx_signing.key -O - | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ABF5BD827BD9BF62
 
 # install
 sudo DEBIAN_FRONTEND=noninteractive apt -q -y update
@@ -20,3 +23,5 @@ sudo DEBIAN_FRONTEND=noninteractive apt -q -y install nginx
 sudo systemctl unmask nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
+
+# add new site
